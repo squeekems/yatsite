3ec2be57f19c5ca5a0af28a4c4649609d9180ba7
Jack Kern <jackrkern@gmail.com>
2023-09-24 17:09:10 -0400

    Correct PlayerController

--- backend/YAT/src/main/java/com/squeekems/yat/controllers/PlayerController.java
+++ backend/YAT/src/main/java/com/squeekems/yat/controllers/PlayerController.java
@@ -4,17 +4,19 @@ import com.squeekems.yat.entities.Event;
 import com.squeekems.yat.entities.Player;
 import com.squeekems.yat.services.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/players")
+@RestController
+@RequestMapping("/players")
 public class PlayerController {
 
   @Autowired
   private PlayerService playerService;
 
-  @RequestMapping("/post")
+  @PostMapping("/post")
   public void postPlayer(@RequestParam("room") int room,
                          @RequestParam("username") String username) {
     Player player = new Player(room, username);
