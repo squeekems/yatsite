5ea9a848b0d52eeb05497036e219a83f51994daf
Jack Kern <jackrkern@gmail.com>
2023-09-24 03:48:25 -0400

    Add PlayerController

--- frontend/yat/src/layouts/homepage/EventCard/EventCard.tsx
+++ frontend/yat/src/layouts/homepage/EventCard/EventCard.tsx
@@ -3,6 +3,7 @@ import EventModel from "../../../models/EventModel";
 import OptionModel from "../../../models/OptionModel"
 import { OptionButton } from "../OptionButton/OptionButton"
 import "./EventCard.css";
+import ReactDOM, { render } from "react-dom";
 
 export const EventCard = (props: {event: EventModel, setEvent: Function}) => {
 
@@ -24,7 +25,43 @@ export const EventCard = (props: {event: EventModel, setEvent: Function}) => {
     loadedEvent.options.sort((a, b) => a.optionId - b.optionId);
 
     console.log(responseJson);
+    switch (eventId) {
+      case 310:
+      case 311:
+      case 312:
+      case 313:
+        return (
+          <div className='main'>
+            <div className='box-div'>
+              <div className='event-card'>
+                <div>
+                  <h1 className='card-title display-1 mb-5'>You're in a Tavern</h1>
+                  <h5 className='card-text display-2 mb-5'>{props.event.prompt}</h5>
+                </div>
+                <div className="d-flex justify-content-around">
+                  <div className='button-container d-flex flex-column'>
+                    <form onSubmit={handleSubmit}>
+                      <input type="text" id="username" name="username" value={username} 
+                        onChange={event => setUsername(event.target.value)}></input>
+                      <button type="submit">Submit</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+    };
     props.setEvent(loadedEvent);
+  }
+  const [username, setUsername] = useState('');
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(event.currentTarget.elements);
+    console.log(event.currentTarget.elements[0]);
+    // const baseUrl: string = 'http://localhost:8080/players/post?';
+    // const url: string = `${baseUrl}room=${props.optionResultId}username=${props.username}`;
   }
 
   return (
