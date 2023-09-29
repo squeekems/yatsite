f550c17c4a4e824c472af9674561a0fe09045af3
Victoria Rhinebolt <vward@hawaii.edu>
2023-09-28 21:32:22 -0400

    get introduction component to display intro sentence

--- frontend/yat/src/components/Introduction/Introduction.tsx
+++ frontend/yat/src/components/Introduction/Introduction.tsx
@@ -1,21 +1,51 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { Message } from "../Message/Message";
 import { ConinueButton } from '../ContinueButton/ContinueButton';
 
 export const Introduction = (
   {
-    gameIntro,
     startGame
   }:
     {
-      gameIntro: string,
       startGame: () => void
     }
 ) => {
   const [displayUsernameInput, setDisplayUsernameInput] = useState(false);
   const [showGreetTraveler, setShowGreetTraveler] = useState(false);
   const [username, setUsername] = useState('');
+  const [showIntro, setShowIntro] = useState(false);
+  const [httpError, setHttpError] = useState('');
+  const [intro, setIntro] = useState('');
+
+  useEffect(() => {
+    getIntro();
+  }, []);
+
+  const getIntro = async (): Promise<void> => {
+    try {
+      const url: string = `http://localhost:8080/game/intro`;
+      console.log('intro url', url)
+      const response = await fetch(url);
+      console.log(response)
+      setIntro(await processIntro(response));
+    } catch (error) {
+      console.log(`Failed to fetch from database`, error);
+    }
+  }
+
+  const processIntro = async (responseData: Response): Promise<string> => {
+    if (!responseData.ok) {
+      setHttpError('An error has occurred');
+      throw new Error('Something went wrong!');
+    }
+    const responseText = await responseData.text(); //await responseData.json();
+
+    const loadedIntro: string = responseText;
+
+    console.log('loadedIntro', loadedIntro)
+    return loadedIntro;
+  }
 
   return (
     <>
@@ -24,7 +54,7 @@ export const Introduction = (
           displayUsernameInput={displayUsernameInput}
           showGreetTraveler={showGreetTraveler}
           username={username}
-          prompt={gameIntro}
+          prompt={intro}
         />
 
         <div className='d-flex justify-content-around'>
