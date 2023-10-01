44507a3d55781050072ed3253bb1c90814f10dae
Eric Rhinebolt <erhinebo@gmail.com>
2023-09-30 21:17:11 -0400

    fix constants to read from application.properties

--- backend/YAT/src/main/java/com/squeekems/yat/util/Constants.java
+++ backend/YAT/src/main/java/com/squeekems/yat/util/Constants.java
@@ -1,22 +1,32 @@
 package com.squeekems.yat.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.Random;
-
+@Component
 public class Constants {
 
-  public static final String jDBCDriver = "spring.datasource.driverClassName";
-  public static final String dbURL = "spring.datasource.url";
-  public static final String username = "spring.datasource.username";
-  public static final String password = "spring.datasource.password";
+    public static String jDBCDriver;
+    public static String dbURL;
+    public static String username;
+    public static String password;
 
-  public static final String fSaving = "Saving \"%s\"";
-  public static final String fDeleting = "Deleting \"%s\"";
-  public static final String fGettingWithId = "Getting %s with id: %s";
-  public static final String fFindingAll = "Finding all %s";
-  public static final String optionContinue = "Continue...";
-  public static final String skipQueue = "Skip your next turn.";
+    @Autowired
+    private void GetPropertiesBean(@Value("${spring.datasource.driverClassName}") String jDBCDriver,
+                                   @Value("${spring.datasource.url}") String dbURL,
+                                   @Value("${spring.datasource.username}") String username,
+                                   @Value("${spring.datasource.password}") String password) {
+        Constants.jDBCDriver = jDBCDriver;
+        Constants.dbURL = dbURL;
+        Constants.username = username;
+        Constants.password = password;
+    }
+
+    public static final String fSaving = "Saving \"%s\"";
+    public static final String fDeleting = "Deleting \"%s\"";
+    public static final String fGettingWithId = "Getting %s with id: %s";
+    public static final String fFindingAll = "Finding all %s";
+    public static final String optionContinue = "Continue...";
+    public static final String skipQueue = "Skip your next turn.";
 }
