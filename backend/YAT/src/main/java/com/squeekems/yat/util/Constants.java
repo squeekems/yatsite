f9e96a05cb7484e4870b56e5d870bf573ebd70bd
Eric Rhinebolt <erhinebo@gmail.com>
2023-09-30 21:33:33 -0400

    add component annotation

--- backend/YAT/src/main/java/com/squeekems/yat/util/Constants.java
+++ backend/YAT/src/main/java/com/squeekems/yat/util/Constants.java
@@ -2,7 +2,9 @@ package com.squeekems.yat.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Constants {
   public static final String CORS_URL = "http://localhost:3000";
   public static String jDBCDriver;
